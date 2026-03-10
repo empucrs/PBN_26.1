9979e4aef41f31a36750cf5f9a36824a10db4afb
Edson Moreno <51421368+empucrs@users.noreply.github.com>
2026-03-10 13:50:03 +0000

    Exercicio de codificação da primeira aula

---
+++ Aula0310-helloworld/src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int valor1, valor2;
+    printf("Informe dois valores-> valor1 valor2: ");
+    scanf("%d %d", &valor1, &valor2);
+    printf("O valor1 digitado foi %d \n", valor1);
+    printf("O valor2 digitado foi %d \n", valor2);
+    return 0;
+}
